4e592773081f1fd3b13e052b2f34ff917072c2ca
Rui Guo <zkyguo123@gmail.com>
2022-09-23 01:22:39 -0400

    Add Game UI C++ classes
    - Finalize all UI C++ classes

---
+++ Source/pj_Arknight/Public/UI/Common/UMG/GameSettings/UI_GameAudioSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Core/UI_RuleOfTheWidget.h"
+#include "UI_GameAudioSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PJ_ARKNIGHT_API UUI_GameAudioSettings : public UUI_RuleOfTheWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/pj_Arknight/Public/UI/GameUI/Core/RuleOfHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "RuleOfHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PJ_ARKNIGHT_API ARuleOfHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
